21832fee82c0da6608a06f2fd86eef559bd00952
bretislav.wajtr <bretislav.wajtr@ibacz.eu>
2016-03-06 17:54:00 +0100

    Moved jooq generator configuration to pom.xml, so the classes can be generated using maven
    Just run "mvn jooq-codegen:generate" to regenerate the classes

--- jooqgenerator/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Download all classpath jars first
-java -cp jooq-3.7.2.jar:jooq-meta-3.7.2.jar:jooq-codegen-3.7.2.jar:postgresql-9.1-901-1.jdbc4.jar org.jooq.util.GenerationTool test-schema.xml
