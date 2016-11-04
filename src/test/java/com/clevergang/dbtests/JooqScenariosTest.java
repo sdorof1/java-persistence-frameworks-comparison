2d0efc7f5e0f9a259795d95746e5aef8bc72b080
bwajtr <bretislav.wajtr@clevergang.com>
2016-11-04 11:53:35 +0100

    Changed test classes annotations regarding context configuration, so the Idea IDE correctly picks up associated context and provides correct code completion (and shows no warnings :))

--- src/test/java/com/clevergang/dbtests/JooqScenariosTest.java
+++ src/test/java/com/clevergang/dbtests/JooqScenariosTest.java
@@ -7,11 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DbTestsApplication.class)
+@SpringBootTest
+@ContextConfiguration(classes = DbTestsApplication.class)
 @Transactional
 @Rollback
 public class JooqScenariosTest {
